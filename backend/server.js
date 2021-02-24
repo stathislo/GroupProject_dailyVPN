ef2b22e2da85695f6761aa315a28b2fa69005a57
alex42q <71628192+alex42q@users.noreply.github.com>
2021-02-25 01:33:57 +0200

    second push

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,26 @@ const PORT = 5000;
 const db = require("./lib/db")
 const bodyParser = require("body-parser")
 const session = require("express-session")
+const MongoDBStore = require("connect-mongodb-session")(session)
+
+
+const store = new MongoDBStore({
+    uri:"mongodb://localhost:27017/vpndaily",
+    collection:"sessions"
+})
+
+app.use(session({
+    secret:"vpndaily",
+    saveUninitialized:false,
+    resave:false,
+    cookie:{maxAge:17760000}
+}))
+
+app.use(cors({
+    origin:"http://localhost:3000",
+    methods:"GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials:true,
+}))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended:false }))
