06f2d856921d78188e77a09212f51dc1169de348
Abarthdev7 <73849212+Abarthdev7@users.noreply.github.com>
2021-02-25 20:57:49 +0200

    Fonts

--- frontend/src/Components/Index/Index.js
+++ frontend/src/Components/Index/Index.js
@@ -24,7 +24,7 @@ export default class Index extends Component {
                 <div id="navbar_container">
                     <nav className="navbar navbar-expand-lg navbar-light ">
                         <div className="container-fluid">
-                            <a className="navbar-brand" href="#">DailyVPN</a>
+                            <a className="navbar-brand" href="#" style={{ color: '#FFF' }} >DailyVPN</a>
                             <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
                                 aria-label="Toggle navigation">
@@ -34,31 +34,29 @@ export default class Index extends Component {
                             </button>
                             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                                 <div className="navbar-nav ms-auto">
-                                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-                                    <a className="nav-link" href="#">Features</a>
-                                    <a className="nav-link" href="#">Pricing</a>
-                                    <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                                    <a className="nav-link active" aria-current="page" href="#" style={{ color: '#FFF' }}>Home</a>
+                                    <a className="nav-link"  href="#" style={{ color: '#FFF' }}>Features</a>
+                                    <a className="nav-link" href="#" style={{ color: '#FFF' }}>Pricing</a>
                                 </div>
                             </div>
                         </div>
                     </nav>
-                    <div>   
-                    <header className="main-header row pos">
-                        <div className="col-lg-6 col-md-6 col-xs-6" id="title-container">
-                            <h1 id="title-header1">High Speed Vpn That Just Rocks </h1>
-                            <h2 id="title-header2">Stay Fast Stay Furious</h2>
+                    <div className="main-header row pos">                         
+                            <div className="col-lg-6" id="title-container">
+                                <h1 id="title-header1">High Speed Vpn That Just Rocks </h1>
+                                <h2 id="title-header2">Stay Fast Stay Furious</h2>
 
-                            <button onClick={this.onButtonClick} type="button" className="btn btn-danger btn-lg" id="btn">Get Your Year Plan+</button>
-                        </div>
-
-                    </header>
+                                <button onClick={this.onButtonClick} type="button" className="btn btn-danger btn-lg" id="btn">Get Your Year Plan+</button>
+                            </div>
+                    
                     </div>
                 </div>
 
 
 
                 <div className='dokimi' id='dokimi'>
-                    test
+                  
+                    hi
                 </div>
 
             </div>
