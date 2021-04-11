e24dc7f245c7471fa07ab54f5fcb3ef890f4fe9c
alex42q <79609827+vpndaily@users.noreply.github.com>
2021-04-11 22:23:38 +0300

    Update ifUserIsLoggedIn.js

--- frontend/src/Components/Logout/IfUserIsLoggedIn/ifUserIsLoggedIn.js
+++ frontend/src/Components/Logout/IfUserIsLoggedIn/ifUserIsLoggedIn.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class ifUserIsLoggedIn extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+
+        }
+        axios.get("http://localhost:5000/main" , { withCredentials:true })
+        .then(res=>{
+            if(res.data.loggedin==="loggedin"){
+                
+               }
+               else{
+                   this.props.history.push("/login")
+               }
+           })
+           .catch(err=>{
+               console.log(err)
+           }) 
+    }
     render() {
         return (
             <div>
